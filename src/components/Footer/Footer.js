350b36773ec546bdb71f36054b25a0c461a092e9
Aleksey Gavrikov <a.e.gavrikoff@gmail.com>
2022-10-15 09:10:24 +0300

    refactor: add screen size limiter for 1280 brakepoint

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,22 +4,24 @@ function Footer() {
 
     return (
         <footer className="footer">
-            <p className="footer__annotation">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className="footer__size-limiter">
+                <p className="footer__annotation">Учебный проект Яндекс.Практикум х BeatFilm.</p>
 
-            <div className="footer__container">
-                <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
-                    <ul className="footer__link-list">
-                        <li>
-                            <a className="footer__link-name" href="https://practicum.yandex.ru/" target="_blank" rel="noopener noreferrer">
-                                Яндекс.Практикум
-                            </a>
-                        </li>
-                        <li>
-                            <a className="footer__link-name" href="https://github.com/Methanoy" target="_blank" rel="noopener noreferrer">
-                                Github
-                            </a>
-                        </li>
-                    </ul>
+                <div className="footer__container">
+                    <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+                        <ul className="footer__link-list">
+                            <li>
+                                <a className="footer__link-name" href="https://practicum.yandex.ru/" target="_blank" rel="noopener noreferrer">
+                                    Яндекс.Практикум
+                                </a>
+                            </li>
+                            <li>
+                                <a className="footer__link-name" href="https://github.com/Methanoy" target="_blank" rel="noopener noreferrer">
+                                    Github
+                                </a>
+                            </li>
+                        </ul>
+                </div>
             </div>
         </footer>
         );
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -5,14 +5,16 @@ function Promo() {
 
   return (
     <article className="promo">
-        <div className="promo__text-container">
-            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-            <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-            <button className="promo__btn">Узнать больше</button>
-        </div>
-        <div className="promo__img-container">
-            <img className="promo__img" src={promoLogo} alt="Изображение глобуса, нарисованного из латинских слов 'web'" />
-        </div>
+      <div className="promo__size-limiter">
+          <div className="promo__text-container">
+              <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+              <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+              <button className="promo__btn">Узнать больше</button>
+          </div>
+          <div className="promo__img-container">
+              <img className="promo__img" src={promoLogo} alt="Изображение глобуса, нарисованного из латинских слов 'web'" />
+          </div>
+      </div>
     </article>
   );
 }
